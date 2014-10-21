5daa641ccf8f3e63c59306a6c000cf2bc373da72
keitaaaaa <wkeitaorange@gmail.com>
2014-10-22 02:33:36 +0900

    edit PostsController.php

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,13 +1,20 @@
 <?php
 class PostsController extends AppController {
-	public $helper = array('Html', 'Form');
+	public $helper = array('Html', 'Form', 'Session');
+	public $components = array('Session');
 
 	public function index() {
 		$this->set('posts', $this->Post->find('all'));
 	}
 
 	public function view($id) {
+		if (!$id) {
+			throw new NotFoundException(__('Invalid post'));
+		}
 		$post = $this->Post->findById($id);
+		if (!$post) {
+			throw new NotFoundException(__('Invalid post'));
+		}
 		$this->set('post', $post);
 	}
 
@@ -15,18 +22,35 @@ class PostsController extends AppController {
 		if($this->request->is('post')) {
 			$this->Post->create();
 			if($this->Post->save($this->request->data)) {
+				$this->Session->setFlash(__('Your post has been saved.'));
 				return $this->redirect(array('action' => 'index'));
 			}
+			$this->Session->setFlash(__('Unable to add your post.'));
 		}
 	}
 
 	public function edit($id) {
+		if (!$id) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+
 		$post = $this->Post->findById($id);
+		
+		if (!$post) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+
 		if($this->request->is(array('post', 'put'))) {
 			$this->Post->id = $id;
 			if($this->Post->save($this->request->data)) {
+				$this->Session->setFlash(__('Your post has been updated.'));
 				return $this->redirect(array('action' => 'index'));
 			}
+			$this->Session->setFlash(__('Unable to update your post.'));
+		}
+		
+		if (!$this->request->data) {
+			$this->request->data = $post;
 		}
 	}
 }
