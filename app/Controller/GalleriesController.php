7ad48115cb7fc42e6a2e7deb5f022d8ee7edb5da
keitaaaaa <wkeitaorange@gmail.com>
2014-11-27 14:54:20 +0900

    delete files

--- app/Controller/GalleriesController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class GalleriesController extends AppController {
-	public $helper = array('Html', 'Form', 'Session');
-	public $components = array('Session');
-
-	public function index() {
-		// $this->set('galleries', $this->Gallery->find('all'));
-	}
-}
